0279f9f21923dd1c8c38185ba36da19796b63301
Maycon Douglas <maycondss@live.com>
2023-12-09 19:03:45 +0000

    hello world

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -3,5 +3,26 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Hello" + " World");
+
+        //Declaring Variables
+        int     num1 = 12;
+        float   num2 = 5.2f;
+        double  num3 = 3.1415;
+        long    num4 = long.MaxValue;
+        long    num5 = long.MinValue;
+        char    mchar = 'b';
+        string  lchar = "string";
+
+
+        Console.WriteLine(num1);
+        Console.WriteLine(num2);
+        Console.WriteLine(num3);
+        Console.WriteLine(num4);
+        Console.WriteLine(num5);
+        Console.WriteLine(mchar);
+        Console.WriteLine(lchar);
+        
+
+
     }
 }
